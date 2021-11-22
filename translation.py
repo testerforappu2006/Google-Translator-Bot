6f9e94d61a19d44e422151acc3b058efe1ede2ab
testerforappu2006 <94781974+testerforappu2006@users.noreply.github.com>
2021-11-22 18:39:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,7 +4,7 @@ class Translation(object):
 
     TRANSLATED_MSG = """Choose The language From here That I Want to Translate.👇"""
 
-    ABOUT_MSG = """🤖 Bot : Google Translator\n\n👨‍🎓 Credit : @PR0FESS0R_99\n\n🎙️ Language : Python3\n\n📚 Lybrary : Pyrogram v1.2.9\n\n🛑 Server : Heroku\n\n📱 Build :V0.4"""
+    ABOUT_MSG = """🤖 Bot : Google Translator\n\n👨‍🎓 Credit : @thegreatmoviecentre\n\n🎙️ Language : Python3\n\n📚 Lybrary : Pyrogram v1.2.9\n\n🛑 Server : Heroku\n\n📱 Build :V0.4"""
 
     CREDITS = """Credits Here"""
 
